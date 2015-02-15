afd9916e8d65422753048a6856f3a0861b51f969
motadou <motadou@126.com>
2015-02-15 23:45:37 +0800

    初始化代码

--- src/com/example/android/BluetoothChat/BluetoothChat.java
+++ src/com/example/android/BluetoothChat/BluetoothChat.java
@@ -185,8 +185,7 @@ public class BluetoothChat extends Activity {
 
     private void ensureDiscoverable() {
         if(D) Log.d(TAG, "ensure discoverable");
-        if (mBluetoothAdapter.getScanMode() !=
-            BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+        if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
             Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
             discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
             startActivity(discoverableIntent);
